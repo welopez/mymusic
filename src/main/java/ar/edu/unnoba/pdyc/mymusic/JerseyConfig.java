f229b21ea75ccfc94e255f69bec826bbfbdea2fc
unknown <walter_lopez@ioma.gba.gov.ar>
2022-04-19 09:35:47 -0300

    Agregado de la clase JerseyConfig

---
+++ src/main/java/ar/edu/unnoba/pdyc/mymusic/JerseyConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ar.edu.unnoba.pdyc.mymusic;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Lenovo
+ */
+@Component
+public class JerseyConfig extends ResourceConfig {
+    
+}
