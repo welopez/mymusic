e2915d7e6ab538d0f277333b18ea8cee8dca5354
unknown <walter_lopez@ioma.gba.gov.ar>
2022-05-03 12:11:20 -0300

    Estructura principal

---
+++ src/main/java/ar/edu/unnoba/pdyc/mymusic/model/Song.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ar.edu.unnoba.pdyc.mymusic.model;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Song {
+    
+}
